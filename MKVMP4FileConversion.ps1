1ef3477e477cba6db4f4c91499608672bb3f4897
GodMode86 <sean.berry-pavaday@outlook.com>
2023-08-06 19:10:14 +0100

    new repo

--- MKVMP4FileConversion.ps1
+++ MKVMP4FileConversion.ps1
@@ -1,8 +1,5 @@
 ﻿# Creator: Bang Em
-# Version: 2.1
-
-# Converts MKV files into MP4 and deletes the original MKV once conversion is complete.
-#====================================================================================================================
+# Version: 2.2 (Remuxes MKV files into MP4)
 
 param (
     [string]$ffmpegPath = "C:\FFMPEG\bin\ffmpeg.exe",
@@ -22,17 +19,11 @@ if (-not (Test-Path $outputDir)) {
     New-Item -ItemType Directory -Path $outputDir | Out-Null
 }
 
-# Function to execute FFmpeg command
-function ConvertToMp4($inputFile, $outputFile) {
-    # Increase the video bitrate (e.g., 8000k) and audio bitrate (e.g., 320k) for higher quality
+# Function to remux MKV file to MP4 format using FFmpeg
+function RemuxToMp4($inputFile, $outputFile) {
     $ffmpegArgs = @(
         "-i", $inputFile,
-        "-c:v", "libx264",
-        "-b:v", "100000k",  # Set the video bitrate (adjust as needed)
-        "-preset", "veryfast",
-        "-c:a", "aac",
-        "-b:a", "320k",   # Set the audio bitrate (adjust as needed)
-        "-strict", "experimental",
+        "-c", "copy",        # Use copy option for remuxing
         $outputFile
     )
     & $ffmpegPath @ffmpegArgs
@@ -43,13 +34,13 @@ $mkvFiles1 = Get-ChildItem -Path $inputDir1 -Filter "*.mkv" -Force
 $mkvFiles2 = Get-ChildItem -Path $inputDir2 -Filter "*.mkv" -Force
 $mkvFiles = @($mkvFiles1) + @($mkvFiles2)
 
-# Initialize a counter for successful conversions
+# Initialize a counter for successful remuxes
 $successCount = 0
 
 # Initialize an array to store error messages
 $errors = @()
 
-# Loop through each MKV file
+# Loop through each MKV file and remux to MP4
 foreach ($mkvFile in $mkvFiles) {
     try {
         # Get the base file name without the extension
@@ -63,40 +54,37 @@ foreach ($mkvFile in $mkvFiles) {
 
         # Check if the output file already exists
         if (Test-Path $outputFile) {
-            Write-Warning "Output file $outputFile already exists. Skipping conversion."
+            Write-Warning "Output file $outputFile already exists. Skipping remuxing."
             continue
         }
 
         # Remux the MKV file into MP4 using FFmpeg
-        ConvertToMp4 $inputFile $outputFile
-
-        # Delete the original MKV file
-        #Remove-Item $inputFile -Force
+        RemuxToMp4 $inputFile $outputFile
 
         # Increment the success counter
         $successCount++
 
-        # If verbose mode is enabled, write conversion details to verbose output
+        # If verbose mode is enabled, write remuxing details to verbose output
         if ($PSBoundParameters["Verbose"]) {
-            Write-Verbose "Converted: $inputFile => $outputFile"
+            Write-Verbose "Remuxed: $inputFile => $outputFile"
         }
     }
     catch {
         # Add the error message to the array
-        $errorMessage = "Error converting file: $mkvFile`nError: $_"
+        $errorMessage = "Error remuxing file: $mkvFile`nError: $_"
         Write-Error $errorMessage
         $errors += $errorMessage
     }
 }
 
-# Log the conversion summary
-Write-Host "Conversion completed."
-Write-Host "Successful conversions: $successCount"
-Write-Host "Failed conversions: $($errors.Count)"
+# Log the remuxing summary
+Write-Host "Remuxing completed."
+Write-Host "Successful remuxes: $successCount"
+Write-Host "Failed remuxes: $($errors.Count)"
 
 # Log any errors encountered
 if ($errors.Count -gt 0) {
-    Write-Host "Conversion errors:"
+    Write-Host "Remuxing errors:"
     $errors | ForEach-Object {
         Write-Host $_
     }
